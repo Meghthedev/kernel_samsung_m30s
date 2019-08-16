0610cdcf44fa9d2e6d93152ef229996fdb76a4a8
Daniel Borkmann <daniel@iogearbox.net>
2019-08-16 23:04:32 +0100

    bpf: get rid of pure_initcall dependency to enable jits
    commit fa9dd599b4dae841924b022768354cfde9affecb upstream.
    
    Having a pure_initcall() callback just to permanently enable BPF
    JITs under CONFIG_BPF_JIT_ALWAYS_ON is unnecessary and could leave
    a small race window in future where JIT is still disabled on boot.
    Since we know about the setting at compilation time anyway, just
    initialize it properly there. Also consolidate all the individual
    bpf_jit_enable variables into a single one and move them under one
    location. Moreover, don't allow for setting unspecified garbage
    values on them.
    
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Acked-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    [bwh: Backported to 4.14 as dependency of commit 2e4a30983b0f
     "bpf: restrict access to core bpf sysctls":
     - Adjust context]
    Signed-off-by: Ben Hutchings <ben.hutchings@codethink.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    # Conflicts:
    #	kernel/bpf/core.c
    #	net/core/sysctl_net_core.c

--- kernel/bpf/core.c
+++ kernel/bpf/core.c
@@ -294,7 +294,10 @@ struct bpf_prog *bpf_patch_insn_single(struct bpf_prog *prog, u32 off,
 int bpf_jit_enable   __read_mostly = IS_BUILTIN(CONFIG_BPF_JIT_ALWAYS_ON);
 int bpf_jit_harden   __read_mostly;
 int bpf_jit_kallsyms __read_mostly;
+<<<<<<< HEAD
 long bpf_jit_limit   __read_mostly;
+=======
+>>>>>>> 234646dcfc5f (bpf: get rid of pure_initcall dependency to enable jits)
 
 static __always_inline void
 bpf_get_prog_addr_region(const struct bpf_prog *prog,
